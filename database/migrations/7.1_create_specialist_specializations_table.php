fcd611401f821c96601d6acc3c08d5b646610291
filippenokdanil <danil-filippenok@mail.ru>
2024-09-12 16:46:57 +0500

    .

---
+++ database/migrations/7.1_create_specialist_specializations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /* 2.1. Специализации специалистов                     */
+        /* ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾*/
+        // 1) id - уникальный идентификатор
+        // 2) id_specialist - ссылка на id специалиста
+        // 3) id_specialization - ссылка на id специализации
+        // 4) category - категория специализации
+        Schema::create('specialist_specializations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_specialist');
+            $table->unsignedBigInteger('id_specialization');
+            $table->string('category', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('specialist_specializations');
+    }
+};
